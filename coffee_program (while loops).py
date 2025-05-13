9b3e93df3844e62634a526f6d63fa47947bffb33
Jurgsl <60768303+Jurgsl@users.noreply.github.com>
2025-05-14 11:03:08 +1200

    Add files via upload
    Version 3. Updated the code. Added a function to run a program to ask a user for a nr.

--- coffee_program (while loops).py
+++ coffee_program (while loops).py
@@ -1,9 +1,10 @@
-# Demonstrate how to use while loops and if statements
+# Demonstrate how to create function, thus making my recyclable.
 # Juergen Lier
-# 9 May 2025
-# Version 2
+# 14 May 2025
+# Version 3
 
 # While loop
+# Version 1
 '''keep_going  = "" # The variable contains an empty string
 while keep_going == "":
 
@@ -19,7 +20,7 @@ while keep_going == "":
         '''
 
 # Version 2. Making the program more pythonic.
-keep_going = ""
+'''keep_going = ""
 while keep_going == "":
     # .lower converts the answer to lower case
     like_coffee = input("Do you like coffee?").lower() 
@@ -42,9 +43,70 @@ while keep_going == "":
         print("I don't understand. Please answer with either Yes or No.")
 
     keep_going = input("Press <enter> to continue or any other key to quit.")
-   
+   '''
 
-    
+# Version 3
+# Create a function that I can use over and over again.
+# Makes my code recyclable.
+# The program will ask a person for a number and do something with it.
+# Loop the program until a valid number gets entered. 
+
+'''def intcheck(): # def creates a function. intcheck is the function name.
+    valid = False
+    while not valid:
+        error = "Whoops, you have entered the wrong number. Please " \
+        "enter a valid integer between 1 and 10."
+
+        try:
+            response = int(input("Enter and integer between 1 and 10: "))
+            
+            if response >=1 and response <=10:
+                print(response)
+                valid = True
+            else: 
+                print(error)
+
+        except ValueError:
+            print(error)
 
 
-    
+intcheck() # To call(use) the function, write out its name.
+'''
+
+# Version 3.1
+# Create a function that I can use over and over again.
+# Makes my code recyclable.
+# The program will ask a person for a number and do something with it.
+# Loop the program until a valid number gets entered. 
+
+def intcheck(question, low, high): # def creates a function. intcheck is the function name.
+    valid = False
+    while not valid:
+        error = f"Whoops, you have entered the wrong number. Please " \
+        f"enter a valid integer between {low} and {high}."
+
+        try:
+            response = int(input(f"Enter and integer between {low} and {high}: "))
+            
+            if low <= response <= high:
+                print(f"You have entered {response}.")
+                valid = True
+            else: 
+                print(error)
+
+        except ValueError:
+            print(error)
+    return response
+
+# Questions to ask the user
+num1 = intcheck("Enter a number between 1 and 10", 1, 10) # To call(use) the function, write out its name.
+num2 = intcheck("Enter a number between 15 and 20", 15, 20)
+# print(num1) # Used to check for correct storage.
+#adding the responses together
+sum_num = num1 + num2
+
+# multiply the responses
+multiply = num1 * num2 
+
+print(f"Your two numbers added together are {sum_num}.")
+print(f"Your two numbers multiplied result in {multiply}.")
